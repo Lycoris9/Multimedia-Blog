936a4b174bdce0d0a9e55d55964320758dcc3a71
AravASC22 <akodali@stu.allstarcode.org>
2022-09-25 11:57:36 -0400

    css

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Week 3
+CSS Cards/Flip cards
+Add buttons to link to levels.fyi sites
+Week 4
+Fix flow of website - having everything on a single page, with links to each section, so that as you scroll down you see a sample of all of the pages
+Week 5
+Add a sources page
+If there’s time:
+Add more pages/content
+Mailchimp newsletter(?)
+lincoln@allstarcode.org
+*/
