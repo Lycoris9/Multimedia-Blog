58b519eaea74ddbca48cc53d6b7462d9e9808126
AravASC22 <akodali@stu.allstarcode.org>
2022-10-06 16:15:01 -0400

    nearly there

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-window.onscroll = function() {stick()};
+/*
 
-var navbar = document.getElementById("big_div")
-var sticky = navbar.offsetTop;
-
-function stick() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-/*Week 3
-CSS Cards/Flip cards
-Add buttons to link to levels.fyi sites
-Week 4
-Fix flow of website - having everything on a single page, with links to each section, so that as you scroll down you see a sample of all of the pages
-Week 5
-Add a sources page
-If there’s time:
-Add more pages/content
-Mailchimp newsletter(?)
-
-
-Put css cards into flexbox to make items be in line
 Formatting - everything should look consistent
 Make the text more clean and legible, maybe a bit larger
 When it goes from a desktop to a mobile environment it should look native
