7ba93a89c58d14dfb2ea0ee595abc9f5f8641cf2
AravASC22 <akodali@stu.allstarcode.org>
2022-10-06 16:42:22 -0400

    script has final two tasks

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 /*
 
-Formatting - everything should look consistent
-Make the text more clean and legible, maybe a bit larger
-When it goes from a desktop to a mobile environment it should look native
-Resize/create logos to be uniform size
-For css cards, have different color text for links
+TO DO:
+CSS Cards + Video appearing in front of nav bar
+Contact Us
 
 */
